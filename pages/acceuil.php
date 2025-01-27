6da3dba424fecd7b47c8f25744cea88362ab7e03
barry <ibrahimbarryib67@gmail.com>
2025-01-27 09:45:48 +0100

    adding and item to the home page

--- pages/acceuil.php
+++ pages/acceuil.php
@@ -1,9 +1,6 @@
-<br/>
-Bienvenue à Arras, joyau des Hauts-de-France<br/>
-Découvrez Arras, une ville au riche patrimoine historique et à l’atmosphère chaleureuse. Célèbre pour ses magnifiques places baroques, <br/>
-son beffroi classé à l’UNESCO et ses souterrains chargés d’histoire, Arras est une destination incontournable pour les amoureux de culture,<br/> 
-d’architecture et de gastronomie.<br/>
-Que vous soyez ici pour une visite, un séjour ou simplement par curiosité, notre site vous guide à travers les trésors de la ville : événements, <br/>
-monuments, bonnes adresses et anecdotes historiques.<br/>
-<br/>
-Explorez et visitez Arras !<br/>
+<section id="acceuil">
+    <h2>Acceuil</h2>
+    <p>Bienvenue à Arras, joyau des Hauts-de-France</p>
+    <p>Découvrez Arras, une ville au riche patrimoine historique et à l’atmosphère chaleureuse. Célèbre pour ses magnifiques places baroques, son beffroi classé à l’UNESCO et ses souterrains chargés d’histoire, Arras est une destination incontournable pour les amoureux de culture d’architecture et de gastronomie.</p>
+    <p>ue vous soyez ici pour une visite, un séjour ou simplement par curiosité, notre site vous guide à travers les trésors de la ville : événements, monuments, bonnes adresses et anecdotes historiques.</p>
+</section>
