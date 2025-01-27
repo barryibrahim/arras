b712afaba450c9aa994fec08cdd14613a79e6b23
Michael <gougammichael@gmail.com>
2025-01-27 09:33:47 +0100

    header uptade

--- pages/header.php
+++ pages/header.php
@@ -19,13 +19,13 @@
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="#">A visiter</a>
+          <a class="nav-link" href="index.php?pages=history">Histoire</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Gastronomie</a>
+          <a class="nav-link" href="index.php?pages=monuments">Monuments</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Transport</a>
+          <a class="nav-link" href="index.php?pages=event">Evenement</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Autre</a>
