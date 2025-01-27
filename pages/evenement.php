5971a85c66d4f8bfedea7c9c608760e0fbd45803
barry <ibrahimbarryib67@gmail.com>
2025-01-27 09:27:39 +0100

    ajout des fichiers histoire,evenement,monument

---
+++ pages/evenement.php
@@ -0,0 +1,4 @@
+<section id="evenements">
+    <h2>Evénement culturels</h2>
+    <p>Arras acceuil de noumbreux événements, dont le Festival Arras film Festival, le Marché de Noêl qui attire chaque années des milliers de visiteurs.</p>
+</section>
