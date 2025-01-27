dc8b11cc41350b9586b9bc2a6fa8ce044d03cdf4
Michael <gougammichael@gmail.com>
2025-01-27 09:53:33 +0100

    gitignore

--- DTB/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $dbh = new PDO('mysql:host=localhost;dbname=ARRAS', 'login5650', "QyLUyZKRlSDLhhV");
-} catch (PDOException $e) {
-    $dbh = null;
-    // attempt to retry the connection after some timeout for example
-}
-?>
