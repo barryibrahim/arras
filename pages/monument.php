742464859e09ef118842ee847ac31a20a0d161e0
Ilan <ilan.issad@gmail.com>
2025-01-29 22:27:04 +0100

    small changes in monument.php

--- pages/monument.php
+++ pages/monument.php
@@ -6,30 +6,38 @@
     <h3 class="text-center">La Place des Héros :</h3>
     <figure class="text-center">
         <img src="../public/images/grandeplace.webp" alt="La Place des Héros à Arras, un lieu emblématique entouré de bâtiments historiques" width="900" height="400">
-        <figcaption>La Place des Héros, un site emblématique d'Arras, entouré de bâtiments historiques.</figcaption>
     </figure>
+    
     <p class="text-center">
-        La Place des Héros est l'un des sites les plus emblématiques d'Arras. Elle est entourée de magnifiques bâtiments historiques et accueille de nombreux événements tout au long de l'année. La place est un lieu de rassemblement pour les habitants et les visiteurs, offrant un cadre idéal pour se détendre ou explorer la ville.
+        <br/>
+        La Place des Héros, un site emblématique d'Arras, entouré de bâtiments historiques.<br/>
+        La Place des Héros est l'un des sites les plus emblématiques d'Arras. Elle est entourée de magnifiques bâtiments historiques et accueille de nombreux événements tout au long de l'année.<br/>
+        La place est un lieu de rassemblement pour les habitants et les visiteurs, offrant un cadre idéal pour se détendre ou explorer la ville.
     </p>
     <br/>
 
     <h3 class="text-center">L'Abbaye Saint-Vaast :</h3>
     <figure class="text-center">
         <img src="../public/images/AbbayeSaintVaast.jpg" alt="L'Abbaye Saint-Vaast à Arras, un ancien monastère devenu musée" width="600" height="400">
-        <figcaption>L'Abbaye Saint-Vaast, un ancien monastère devenu musée.</figcaption>
     </figure>
     <p class="text-center">
-        L'Abbaye Saint-Vaast est un ancien monastère devenu musée, où les visiteurs peuvent découvrir l'histoire fascinante de la ville d'Arras. Les superbes caves de la ville, qui faisaient autrefois partie de l'abbaye, offrent un aperçu de l'histoire viticole de la région.
+        <br/>
+        L'Abbaye Saint-Vaast, un ancien monastère devenu musée.<br/>
+        L'Abbaye Saint-Vaast est un ancien monastère devenu musée, où les visiteurs peuvent découvrir l'histoire fascinante de la ville d'Arras.<br/>
+        Les superbes caves de la ville, qui faisaient autrefois partie de l'abbaye, offrent un aperçu de l'histoire viticole de la région.
     </p>
     <br/>
 
     <h3 class="text-center">La Citadelle d'Arras :</h3>
     <figure class="text-center">
         <img src="../public/images/citadelle.jpg" alt="La Citadelle d'Arras, un exemple d'architecture militaire datant du XVIIe siècle" width="600" height="400">
-        <figcaption>La Citadelle d'Arras, un exemple d'architecture militaire datant du XVIIe siècle.</figcaption>
+        <figcaption></figcaption>
     </figure>
     <p class="text-center">
-        La Citadelle d'Arras, datant du XVIIe siècle, est un exemple exceptionnel d'architecture militaire. Elle est située sur une colline et offre une vue imprenable sur la ville et ses environs. C'est un site historique fascinant, riche en histoire et en patrimoine.
+        <br/>
+        La Citadelle d'Arras, un exemple d'architecture militaire datant du XVIIe siècle.<br/>
+        La Citadelle d'Arras, datant du XVIIe siècle, est un exemple exceptionnel d'architecture militaire. Elle est située sur une colline et offre une vue imprenable sur la ville et ses environs. <br/>
+        C'est un site historique fascinant, riche en histoire et en patrimoine.
     </p>
     <br/>
 </section>
