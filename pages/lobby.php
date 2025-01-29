0e04bbc57cd1e22a5d81bb8a15aba5ed22fc9d1d
Michael <gougammichael@gmail.com>
2025-01-29 16:04:22 +0100

    update history acceuil

--- pages/lobby.php
+++ pages/lobby.php
@@ -1,7 +1,9 @@
 <section id="lobby">
-<br/><br/><h1>Acceuil</h1><br/>
-    <p>Bienvenue à Arras, joyau des Hauts-de-France <br/>
-    Découvrez Arras, une ville au riche patrimoine historique et à l’atmosphère chaleureuse. Célèbre pour ses magnifiques places baroques, son beffroi classé à l’UNESCO et ses souterrains chargés d’histoire, Arras est une destination <br/>
-    incontournable pour les amoureux de culture d’architecture et de gastronomie.<br/>
-    Que vous soyez ici pour une visite, un séjour ou simplement par curiosité, notre site vous guide à travers les trésors de la ville : événements, monuments, bonnes adresses et anecdotes historiques.</p>
+<br/><br/><h1 style="text-align: center;">Acceuil</h1><br/>
+    <p style="text-align: center;"><strong>Bienvenue à Arras, joyaux des Hauts-de-France,</strong><br/>
+    <p style="text-align: center;"><br/>Découvrez Arras, une ville au riche patrimoine historique et à l’atmosphère chaleureuse. Célèbre pour ses magnifiques places baroques, son beffroi classé à l’UNESCO et ses souterrains chargés d’histoire, Arras est une destination <br/>
+    incontournable pour les amoureux de culture d’architecture et de gastronomie.<br/><br/>
+    <img src="../public/images/grandeplaceARRAS.webp" alt="grande place" width="750" height="500" style="display: block; margin: 0 auto;"> 
+    <p style="text-align: center;"><br/>Que vous soyez ici pour une visite, un séjour ou simplement par curiosité, notre site vous guide à travers les trésors de la ville : événements, monuments, bonnes adresses et anecdotes historiques.</p>
 </section>
+<br/><br/><br/><br/><br/><br/><br/>
