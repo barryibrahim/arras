455df6b2eb13fa470c11674f5734c48cf7430b7f
barry <ibrahimbarryib67@gmail.com>
2025-01-29 20:54:48 +0100

    adding the figure and figcaption tag

--- pages/monument.php
+++ pages/monument.php
@@ -1,20 +1,35 @@
 <section id="monuments">
-  <br/><br/>
-  <h1>Monuments et Lieux d'Intérêt</h1><br/>
-  <p><strong>Découvrez les monuments représentatifs. Arras est célèbre pour ses monuments historiques, dont la magnifique place des Héros, bordée de maisons en style.</strong></p>
-  
-  <h3>La Place des Héros:</h3>
-  <img src="../public/images/grandeplace.webp" alt="place des héros" width="900" height="400">
-  <p><br/>La Place des Héros est l'un des sites les plus emblématiques d'Arras. Elle est entourée de magnifiques bâtiments historiques et accueille de nombreux événements tout au long de l'année. La place est un lieu de rassemblement pour les habitants et les visiteurs, offrant un cadre idéal pour se détendre ou explorer la ville.</p>
-  
-  <h3>L'Abbaye Saint-Vaast:</h3>
-  <img src="../public/images/AbbayeSaintVaast.jpg" alt="Abbaye Saint-Vaast" width="600" height="400">
-  <p><br/>L'Abbaye Saint-Vaast est un ancien monastère devenu musée, où les visiteurs peuvent découvrir l'histoire fascinante de la ville d'Arras. Les superbes caves de la ville, qui faisaient autrefois partie de l'abbaye, offrent un aperçu de l'histoire viticole de la région.</p>
-  
-  <h3>La Citadelle d'Arras:</h3>
-  <div>
-    <img src="../public/images/citadelle.jpg" alt="Citadelle d'Arras" width="600" height="400">
-  </div>
-  <p ><br/>La Citadelle d'Arras, datant du XVIIe siècle, est un exemple exceptionnel d'architecture militaire. Elle est située sur une colline et offre une vue imprenable sur la ville et ses environs. C'est un site historique fascinant, riche en histoire et en patrimoine.</p>
+    </br><br/><h1 class="text-center">Monuments et Lieux d'Intérêt</h1><br/>
+    <p class="text-center"><strong>Découvrez les monuments emblématiques. Arras est célèbre pour ses monuments historiques, dont la magnifique place des Héros, bordée de maisons dans un style unique.</strong></p>
+    <br/>
+
+    <h3 class="text-center">La Place des Héros :</h3>
+    <figure class="text-center">
+        <img src="../public/images/grandeplace.webp" alt="La Place des Héros à Arras, un lieu emblématique entouré de bâtiments historiques" width="900" height="400">
+        <figcaption>La Place des Héros, un site emblématique d'Arras, entouré de bâtiments historiques.</figcaption>
+    </figure>
+    <p class="text-center">
+        La Place des Héros est l'un des sites les plus emblématiques d'Arras. Elle est entourée de magnifiques bâtiments historiques et accueille de nombreux événements tout au long de l'année. La place est un lieu de rassemblement pour les habitants et les visiteurs, offrant un cadre idéal pour se détendre ou explorer la ville.
+    </p>
+    <br/>
+
+    <h3 class="text-center">L'Abbaye Saint-Vaast :</h3>
+    <figure class="text-center">
+        <img src="../public/images/AbbayeSaintVaast.jpg" alt="L'Abbaye Saint-Vaast à Arras, un ancien monastère devenu musée" width="600" height="400">
+        <figcaption>L'Abbaye Saint-Vaast, un ancien monastère devenu musée.</figcaption>
+    </figure>
+    <p class="text-center">
+        L'Abbaye Saint-Vaast est un ancien monastère devenu musée, où les visiteurs peuvent découvrir l'histoire fascinante de la ville d'Arras. Les superbes caves de la ville, qui faisaient autrefois partie de l'abbaye, offrent un aperçu de l'histoire viticole de la région.
+    </p>
+    <br/>
+
+    <h3 class="text-center">La Citadelle d'Arras :</h3>
+    <figure class="text-center">
+        <img src="../public/images/citadelle.jpg" alt="La Citadelle d'Arras, un exemple d'architecture militaire datant du XVIIe siècle" width="600" height="400">
+        <figcaption>La Citadelle d'Arras, un exemple d'architecture militaire datant du XVIIe siècle.</figcaption>
+    </figure>
+    <p class="text-center">
+        La Citadelle d'Arras, datant du XVIIe siècle, est un exemple exceptionnel d'architecture militaire. Elle est située sur une colline et offre une vue imprenable sur la ville et ses environs. C'est un site historique fascinant, riche en histoire et en patrimoine.
+    </p>
+    <br/>
 </section>
-<br/><br/><br/><br/><br/>
