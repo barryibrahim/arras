760ab12a5a12764c25a113e8a9bdd14df424955f
Ilan <ilan.issad@gmail.com>
2025-01-29 22:04:44 +0100

    added feature that check if the email is allready in the DataBase and other tweak

--- DTB/connect.php
+++ DTB/connect.php
@@ -2,7 +2,6 @@
 require_once 'id.php';
 try {
     $dbh = new PDO('mysql:host='.$serveur.';dbname='.$bd,$login,$password);
-    $user_mail = "";
 } catch (PDOException $e) {
     $dbh = null;
     // attempt to retry the connection after some timeout for example
