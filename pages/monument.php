d63fdd8d2c402724def9be153f5a3b38f794bb62
Michael <gougammichael@gmail.com>
2025-01-29 16:43:54 +0100

    update image monuments

--- pages/monument.php
+++ pages/monument.php
@@ -4,18 +4,17 @@
   <p><strong>Découvrez les monuments représentatifs. Arras est célèbre pour ses monuments historiques, dont la magnifique place des Héros, bordée de maisons en style.</strong></p>
   
   <h3>La Place des Héros:</h3>
-  <img src="../public/images/place%20des%20h%C3%A9ros.jpg" alt="place des héros" width="900" height="400">
+  <img src="../public/images/grandeplace.webp" alt="place des héros" width="900" height="400">
   <p><br/>La Place des Héros est l'un des sites les plus emblématiques d'Arras. Elle est entourée de magnifiques bâtiments historiques et accueille de nombreux événements tout au long de l'année. La place est un lieu de rassemblement pour les habitants et les visiteurs, offrant un cadre idéal pour se détendre ou explorer la ville.</p>
   
   <h3>L'Abbaye Saint-Vaast:</h3>
-  <img src="../public/images/abaye%20saint%20vaast.jpg" alt="Abbaye Saint-Vaast" width="600" height="400">
+  <img src="../public/images/AbbayeSaintVaast.jpg" alt="Abbaye Saint-Vaast" width="600" height="400">
   <p><br/>L'Abbaye Saint-Vaast est un ancien monastère devenu musée, où les visiteurs peuvent découvrir l'histoire fascinante de la ville d'Arras. Les superbes caves de la ville, qui faisaient autrefois partie de l'abbaye, offrent un aperçu de l'histoire viticole de la région.</p>
   
   <h3>La Citadelle d'Arras:</h3>
   <div>
-    <img src="../public/images/citadelle%20d'arras.jpg" alt="Citadelle d'Arras" width="600" height="400">
+    <img src="../public/images/citadelle.jpg" alt="Citadelle d'Arras" width="600" height="400">
   </div>
-  <p><br/>La Citadelle d'Arras, datant du XVIIe siècle, est un exemple exceptionnel d'architecture militaire. Elle est située sur une colline et offre une vue imprenable sur la ville et ses environs. C'est un site historique fascinant, riche en histoire et en patrimoine.</p>
-
- 
+  <p ><br/>La Citadelle d'Arras, datant du XVIIe siècle, est un exemple exceptionnel d'architecture militaire. Elle est située sur une colline et offre une vue imprenable sur la ville et ses environs. C'est un site historique fascinant, riche en histoire et en patrimoine.</p>
 </section>
+<br/><br/><br/><br/><br/>
