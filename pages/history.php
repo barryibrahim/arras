ca29dbeceba70197d80a575efe9d22c874418bf5
barry <ibrahimbarryib67@gmail.com>
2025-01-30 11:30:25 +0100

    nouvelle modification dans les pages suivantes:history,monument,lobby

--- pages/history.php
+++ pages/history.php
@@ -1,30 +1,37 @@
-<section id="history">
-    <br/><br/><h1 style="text-align: center;">Histoire de la ville</h1><br/>
-    <p><strong>Arras est une ville située dans le département du Pas-de-Calais, dans le nord de la France. Elle a une histoire riche et variée, marquée par des événements importants et des périodes de prospérité et de conflits. Voici un aperçu de l’histoire de cette ville :</strong></p>
-<br/>
-<p><h5 style="text-align: center;">1. Antiquité et époque romaine :</h5>
-<p>Arras était à l'origine un village gaulois habité par les Atrebates, un peuple celte. Après la conquête romaine au Ier siècle avant J.-C., la ville devient un centre important de la province romaine de la Belgica, sous le nom de Atuatuca. C'était une ville commerçante et une plaque tournante pour les échanges.<br/>
-<br/><br/>
-<p><h5 style="text-align: center;">2. Moyen Âge :</h5>
-<p>Au Moyen Âge, Arras se développe grâce à sa position stratégique et son commerce florissant. Elle devient une cité prospère au XIe siècle, notamment grâce à l'industrie de la laine et du textile. Arras est également célèbre pour ses tapisseries, qui connaissent un grand succès en Europe. La ville devient un centre religieux et politique majeur.<br/>
-<br/><br/>
-<p><h5 style="text-align: center;">3. Guerre de Cent Ans et période de troubles :</h5>
-<p>Au XIVe siècle, la ville est touchée par la guerre de Cent Ans (1337-1453) entre la France et l'Angleterre. Arras passe plusieurs fois sous contrôle des Anglais, avant d’être reprise par les Français. En 1435, elle devient le lieu de la signature du traité d'Arras, qui marque la fin du conflit entre le royaume de France et le duché de Bourgogne, un événement clé de cette époque.<br/>
-<br/><br/>
-<p><h5 style="text-align: center;">4. Renaissance et domination bourguignonne :</h5>
-<p>Durant la Renaissance, Arras continue de prospérer grâce à son commerce, notamment de tissus et de tapisseries. La ville est influencée par la culture flamande et bourguignonne. Cependant, elle traverse une période de troubles durant les guerres de religion, au XVIe siècle.<br/>
-<br/><br/>
-<p><h5 style="text-align: center;">5. Révolution française et XIXe siècle :</h5>
-<p>Au XVIIIe siècle, Arras, comme beaucoup de villes de France, connaît des tensions sociales et politiques qui culminent avec la Révolution française. La ville est le lieu d'importants événements politiques, et elle devient un centre d'agitation révolutionnaire. Au XIXe siècle, Arras se modernise avec la construction de nouveaux bâtiments et infrastructures.<br/>
-<br/><br/>
-<p><h5 style="text-align: center;">6. Première Guerre mondiale :</h5>
-<p>La Première Guerre mondiale marque un tournant dans l’histoire d’Arras. Située à proximité du front, la ville est lourdement bombardée et subit de graves destructions. Les tranchées, les combats violents, et les pertes humaines marquent profondément la région. Après la guerre, la ville est reconstruite, et de nombreux monuments sont restaurés, y compris le Beffroi et la Basilique Saint-Vaast, qui sont désormais classés au patrimoine mondial de l'UNESCO.<br/>
-<br/><br/>
-<p><h5 style="text-align: center;">7. Aujourd'hui :</h5>
-<p>Aujourd'hui, Arras est une ville dynamique et culturellement riche, qui attire les visiteurs grâce à ses sites historiques et son patrimoine. Elle est particulièrement connue pour sa Grand-Place, ses Places des Héros, et son Beffroi, ainsi que pour sa basilique et ses musées. Arras est aussi le siège de nombreux événements culturels et festivals, comme le Main Square Festival.<br/>
-<br/><br/>
-<p>Arras continue de vivre au rythme de son histoire, entre tradition et modernité, et reste un lieu de mémoire pour les événements marquants du XXe siècle, notamment la Première Guerre mondiale.
+<section id="history" style="text-align: center; line-height: 1.5;">
+    <br/><br/>
+    <h1>Histoire de la ville</h1>
+    <br/>
+    <p><strong>Arras est une ville située dans le département du Pas-de-Calais, dans le nord de la France. Elle possède une histoire riche, façonnée par des événements marquants et des périodes<br/> variées de prospérité et de conflits. Voici un aperçu de cette histoire fascinante :</strong></p>
+    <br/>
+    <h5>1. Antiquité et époque romaine :</h5>
+    <p>Arras était initialement un village gaulois habité par les Atrebates, un peuple celte. Lors de la conquête romaine au Ier siècle avant J.-C., </br>la ville devient un centre stratégique de la province romaine de la Belgica, sous le nom de Atuatuca. C'était une ville commerçante dynamique, idéale pour les échanges entre les peuples.</p>
+    <br/><br/>
 
-</p>
+    <h5>2. Moyen Âge :</h5>
+    <p>Au Moyen Âge, la ville bénéficie de sa position géographique stratégique et devient un pôle commercial important. Au XIe siècle, <br/>elle se distingue pour son industrie de la laine et des tapisseries, qui rencontrent un grand succès en Europe. La ville connaît également une grande prospérité religieuse et politique.</p>
+    <br/><br/>
+
+    <h5>3. Guerre de Cent Ans et période de troubles :</h5>
+    <p>Au XIVe siècle, la guerre de Cent Ans entre la France et l'Angleterre touche directement Arras. <br/>La ville passe plusieurs fois sous le contrôle des Anglais avant d’être récupérée par les Français. En 1435, <br/>Arras devient le lieu de signature du traité du même nom, un accord majeur mettant fin au conflit entre le royaume de France et le duché de Bourgogne.</p>
+    <br/><br/>
+
+    <h5>4. Renaissance et domination bourguignonne :</h5>
+    <p>Durant la Renaissance, la ville prospère grâce à son commerce florissant, notamment de tissus et de tapisseries. <br/>Influencée par les cultures flamande et bourguignonne, Arras traverse une période plus agitée durant les guerres de religion au XVIe siècle.</p>
+    <br/><br/>
+
+    <h5>5. Révolution française et XIXe siècle :</h5>
+    <p>Au XVIIIe siècle, Arras, comme de nombreuses autres villes françaises, traverse une période de tensions sociales et politiques, culminant avec la Révolution française. <br/>La ville devient un centre d'agitation révolutionnaire. Au XIXe siècle, Arras se modernise avec la construction de nouveaux bâtiments et infrastructures.</p>
+    <br/><br/>
+
+    <h5>6. Première Guerre mondiale :</h5>
+    <p>La Première Guerre mondiale constitue un tournant pour Arras, située à proximité du front. <br/>Elle est lourdement bombardée et subit des destructions massives. Les tranchées et les combats violents marquent profondément la région. <br/>Après la guerre, la ville est reconstruite, et des monuments emblématiques tels que le Beffroi et la Basilique Saint-Vaast sont restaurés et classés au patrimoine mondial de l'UNESCO.</p>
+    <br/><br/>
+
+    <h5>7. Aujourd'hui :</h5>
+    <p>Aujourd'hui, Arras est une ville vivante et culturellement riche, attirant les visiteurs grâce à son patrimoine historique. On y trouve des sites comme la Grand-Place, <br/>les Places des Héros, le Beffroi, ainsi que des musées remarquables. La ville est aussi le lieu de nombreux événements, comme le Main Square Festival, qui célèbrent la culture et la diversité.</p>
+    <br/><br/>
+
+    <p>Arras continue de vivre au rythme de son histoire, alliant tradition et modernité, <br/>et reste un lieu de mémoire, en particulier pour les événements marquants du XXe siècle, notamment la Première Guerre mondiale.</p>
 </section>
 <br/><br/><br/><br/><br/><br/>
